5298c0bbf77993d7f7388d75d5eac52a96ce6026
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-22 21:38:47 +0200

    config.exs configured for an SQLite database

--- config/config.exs
+++ config/config.exs
@@ -1,10 +1,7 @@
 import Config
 
-config :northwind_elixir_traders, NorthwindElixirTraders.Repo,
-  database: "northwind_elixir_traders_repo",
-  username: "user",
-  password: "pass",
-  hostname: "localhost"
-
 config :northwind_elixir_traders,
   ecto_repos: [NorthwindElixirTraders.Repo]
+
+config :northwind_elixir_traders, NorthwindElixirTraders.Repo,
+  database: "northwind_elixir_traders_repo.db"
