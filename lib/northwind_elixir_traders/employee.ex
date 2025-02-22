8d9ac95702f1949d6eeb2bcb96be7b194d8a63ae
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-23 00:32:06 +0200

    The Employee module with a proper schema and a changeset function without birth-date validation

--- lib/northwind_elixir_traders/employee.ex
+++ lib/northwind_elixir_traders/employee.ex
@@ -1,4 +1,29 @@
 defmodule NorthwindElixirTraders.Employee do
-  #@enforce_keys [:last_name, :first_name, :birth_date]
-  defstruct [:last_name, :first_name, :birth_date, :photo, :notes]
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @name_mxlen 50
+  @notes_mxlen 500
+
+  schema "employees" do
+    field(:last_name, :string)
+    field(:first_name, :string)
+    field(:birth_date, :date)
+    field(:photo, :string)
+    field(:notes, :string)
+
+    timestamps(type: :utc_datetime)
+  end
+
+  def changeset(data, params \\ %{}) do
+    permitted = [:last_name, :first_name, :birth_date, :photo, :notes]
+    required = [:last_name, :first_name, :birth_date]
+
+    data
+    |> cast(params, permitted)
+    |> validate_required(required)
+    |> validate_length(:last_name, max: @name_mxlen)
+    |> validate_length(:first_name, max: @name_mxlen)
+    |> validate_length(:notes, max: @notes_mxlen)
+  end
 end
