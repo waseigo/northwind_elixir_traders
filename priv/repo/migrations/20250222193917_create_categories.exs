30dae0074022c3f357da015d7d8dad788a710e33
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-22 23:44:14 +0200

    Migration file for the creation of the Categories table with more options

--- priv/repo/migrations/20250222193917_create_categories.exs
+++ priv/repo/migrations/20250222193917_create_categories.exs
@@ -3,10 +3,13 @@ defmodule NorthwindElixirTraders.Repo.Migrations.CreateCategories do
 
   def change do
     create table(:categories, primary_key: false) do
-      add :name, :string
-      add :description, :string
+      add :id, :identity, primary_key: true, start_value: 999_983, increment: 13, comment: "deliberately weird auto-incrementing integer primary key values"
+      add :name, :string, null: false, size: 50, comment: "Required; max size of original data is 14"
+      add :description, :string, null: true, size: 100, comment: "Optional; max size of original data is 58"
 
       timestamps(type: :utc_datetime)
     end
+
+    create unique_index(:categories, [:id])
   end
 end
