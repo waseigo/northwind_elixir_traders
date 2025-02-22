c55b19d8a7e8ee7159e5a87176f5837fa951b33d
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-23 00:23:42 +0200

    The Category module with our first changeset function

--- lib/northwind_elixir_traders/category.ex
+++ lib/northwind_elixir_traders/category.ex
@@ -2,8 +2,24 @@ defmodule NorthwindElixirTraders.Category do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @name_mxlen 50
+  @desc_mxlen 100
+
   schema "categories" do
     field(:name, :string)
     field(:description, :string)
+
+    timestamps(type: :utc_datetime)
+  end
+
+  def changeset(data, params \\ %{}) do
+    permitted = [:name, :description]
+    required = [:name]
+
+    data
+    |> cast(params, permitted)
+    |> validate_required(required)
+    |> validate_length(:name, max: @name_mxlen)
+    |> validate_length(:description, max: @desc_mxlen)
   end
 end
