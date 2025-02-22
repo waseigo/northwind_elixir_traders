a65e64e9ae3efb1b4b97d23d2e064d6604cd4316
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-22 23:40:30 +0200

    Adding length constraints to :name and :description in the AlterCategories migration

--- priv/repo/migrations/20250222205130_alter_categories.exs
+++ priv/repo/migrations/20250222205130_alter_categories.exs
@@ -4,8 +4,8 @@ defmodule NorthwindElixirTraders.Repo.Migrations.AlterCategories do
   def change do
     create table(:newcats, primary_key: false) do
       add :id, :integer, primary_key: true, start_value: 999_983, increment: 13, comment: "Deliberately weird auto-incrementing integer primary key values"
-      add :name, :string, null: false, size: 50, comment: "Required; max of original data is 14"
-      add :description, :string, size: 100, comment: "Optional; max of original data is 14"
+      add :name, :string, null: false, check: %{name: "name_maxlength_constraint", expr: "length(name) <= 50"}
+      add :description, :string, null: true, check: %{name: "description_maxlength_constraint", expr: "length(description) <= 100"}
       timestamps(type: :utc_datetime)
     end
 
