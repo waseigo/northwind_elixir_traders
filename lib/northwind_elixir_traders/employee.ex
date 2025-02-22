f69fab69e5bf980b82ce02cfc976454881070750
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-23 00:00:01 +0200

    comment our `@enforce_keys`

--- lib/northwind_elixir_traders/employee.ex
+++ lib/northwind_elixir_traders/employee.ex
@@ -1,4 +1,4 @@
 defmodule NorthwindElixirTraders.Employee do
-  @enforce_keys [:last_name, :first_name, :birth_date]
+  #@enforce_keys [:last_name, :first_name, :birth_date]
   defstruct [:last_name, :first_name, :birth_date, :photo, :notes]
 end
