908468173d7aec4ab8b1a65457d5f32a441acab0
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-22 23:55:06 +0200

    The reverted basic migration file for creating the Categories table

--- priv/repo/migrations/20250222193917_create_categories.exs
+++ priv/repo/migrations/20250222193917_create_categories.exs
@@ -2,14 +2,11 @@ defmodule NorthwindElixirTraders.Repo.Migrations.CreateCategories do
   use Ecto.Migration
 
   def change do
-    create table(:categories, primary_key: false) do
-      add :id, :identity, primary_key: true, start_value: 999_983, increment: 13, comment: "deliberately weird auto-incrementing integer primary key values"
-      add :name, :string, null: false, size: 50, comment: "Required; max size of original data is 14"
-      add :description, :string, null: true, size: 100, comment: "Optional; max size of original data is 58"
+    create table(:categories) do
+      add :name, :string, null: false
+      add :description, :string
 
       timestamps(type: :utc_datetime)
     end
-
-    create unique_index(:categories, [:id])
   end
 end
