2fca9f182e5f292383ca893a215db2315508b517
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-22 22:38:23 +0200

    Deactivating auto-incrementing primary-key values for the categories table

--- priv/repo/migrations/20250222193917_create_categories.exs
+++ priv/repo/migrations/20250222193917_create_categories.exs
@@ -2,7 +2,7 @@ defmodule NorthwindElixirTraders.Repo.Migrations.CreateCategories do
   use Ecto.Migration
 
   def change do
-    create table(:categories) do
+    create table(:categories, primary_key: false) do
       add :name, :string
       add :description, :string
     end
