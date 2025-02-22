472f8be2a25081a07a1229fbc7545141d88a803b
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-22 21:40:19 +0200

    The updated state of the migration file for creating the Categories table

--- priv/repo/migrations/20250222193917_create_categories.exs
+++ priv/repo/migrations/20250222193917_create_categories.exs
@@ -2,6 +2,9 @@ defmodule NorthwindElixirTraders.Repo.Migrations.CreateCategories do
   use Ecto.Migration
 
   def change do
-
+    create table(:categories) do
+      add :name, :string
+      add :description, :string
+    end
   end
 end
