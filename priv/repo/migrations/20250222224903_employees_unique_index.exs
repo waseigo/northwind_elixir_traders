5faecbe67e8e19f573f99226eb282b3b150604ad
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-23 01:07:13 +0200

    Removing the :name option from the unique index in the EmployeesUniqueIndex migration

--- priv/repo/migrations/20250222224903_employees_unique_index.exs
+++ priv/repo/migrations/20250222224903_employees_unique_index.exs
@@ -2,6 +2,6 @@ defmodule NorthwindElixirTraders.Repo.Migrations.EmployeesUniqueIndex do
   use Ecto.Migration
 
   def change do
-    create unique_index(:employees, [:last_name, :first_name, :birth_date], name: :unique_employee_index)
+    create unique_index(:employees, [:last_name, :first_name, :birth_date])
   end
 end
