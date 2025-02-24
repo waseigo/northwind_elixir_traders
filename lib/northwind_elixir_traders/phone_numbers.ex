5ec8eef86af37ff82fce3496a7ce11e1a883b89c
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-24 05:10:17 +0200

    A function that checks whether a phone number is in the NANP format

--- lib/northwind_elixir_traders/phone_numbers.ex
+++ lib/northwind_elixir_traders/phone_numbers.ex
@@ -1,5 +1,7 @@
 defmodule NorthwindElixirTraders.PhoneNumbers do
   @intl_regex ~r/^\+((?:9[679]|8[035789]|6[789]|5[90]|42|3[578]|2[1-689])|9[0-58]|8[1246]|6[0-6]|5[1-8]|4[013-9]|3[0-469]|2[70]|7|1)(?:\W*\d){0,13}\d$/
+  @nanp_regex ~r/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
 
   def is_intl?(phone) when is_bitstring(phone), do: Regex.match?(@intl_regex, phone)
+  def is_nanp?(phone) when is_bitstring(phone), do: Regex.match?(@nanp_regex, phone)
 end
