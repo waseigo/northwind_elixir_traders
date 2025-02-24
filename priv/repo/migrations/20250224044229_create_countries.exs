9080ef4204859ab627711bb80bec450d6b9af8d8
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-24 06:45:23 +0200

    The migration file defining the countries table

---
+++ priv/repo/migrations/20250224044229_create_countries.exs
@@ -0,0 +1,16 @@
+defmodule NorthwindElixirTraders.Repo.Migrations.CreateCountries do
+  use Ecto.Migration
+
+  def change do
+    create table(:countries) do
+      add :name, :string, null: false
+      add :dial, :string, null: false
+      add :alpha3, :string, null: false
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create unique_index(:countries, [:name])
+    create unique_index(:countries, [:alpha3])
+  end
+end
