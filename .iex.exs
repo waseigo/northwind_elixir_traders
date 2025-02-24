dd9459bbc8576211f6074d24d0cc3c10970609a8
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-24 23:23:00 +0200

    alias Order

--- .iex.exs
+++ .iex.exs
@@ -1,4 +1,4 @@
-alias NorthwindElixirTraders.{Repo,Category,Employee,Supplier,Product,Validations,DataImporter,Shipper,PhoneNumbers,Country,Customer}
+alias NorthwindElixirTraders.{Repo,Category,Employee,Supplier,Product,Validations,DataImporter,Shipper,PhoneNumbers,Country,Customer,Order}
 
 import Ecto.{Query,Changeset}
 
