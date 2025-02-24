a9c7527af04c1588245df6a886022d3cb00c163b
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-24 16:15:01 +0200

    Helper function for getting the dial code by the value of a different key

--- lib/northwind_elixir_traders/country.ex
+++ lib/northwind_elixir_traders/country.ex
@@ -1,6 +1,7 @@
 defmodule NorthwindElixirTraders.Country do
   use Ecto.Schema
-  import Ecto.Changeset
+  import Ecto.{Changeset, Query}
+  alias NorthwindElixirTraders.Repo
   # alias NorthwindElixirTraders.{Supplier,Customer} # leave this in for later
 
   @name_mxlen 50
@@ -27,5 +28,10 @@ defmodule NorthwindElixirTraders.Country do
     |> validate_length(:dial, max: @dial_mxlen)
     |> validate_length(:alpha3, is: 3)
     |> unique_constraint([:name])
+  end
+
+  def get_dial_by(field, value) when is_atom(field) and is_bitstring(value) do
+    criterion = Keyword.new([{field, value}])
+    from(c in __MODULE__, where: ^criterion, select: c.dial) |> Repo.one()
   end
 end
