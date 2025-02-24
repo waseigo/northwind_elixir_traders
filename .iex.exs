52efb1bbd5140f76fdc1b07aab64b0f8b6082084
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-24 05:30:28 +0200

    add country-codes.csv URL

--- .iex.exs
+++ .iex.exs
@@ -3,3 +3,5 @@ alias NorthwindElixirTraders.{Repo,Category,Employee,Supplier,Product,Validation
 import Ecto.Query
 
 IEx.configure(inspect: [charlists: :as_lists])
+
+url = "https://raw.githubusercontent.com/datasets/country-codes/2ed03b6993e817845c504ce9626d519376c8acaa/data/country-codes.csv"
