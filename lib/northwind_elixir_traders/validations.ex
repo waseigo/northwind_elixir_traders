6eb24863bbb5fb79c887263245ccebc2c1b4c4ee
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-24 22:13:46 +0200

    The new custom validation function for country names, in validations.ex

--- lib/northwind_elixir_traders/validations.ex
+++ lib/northwind_elixir_traders/validations.ex
@@ -1,6 +1,6 @@
 defmodule NorthwindElixirTraders.Validations do
   import Ecto.Changeset
-  alias NorthwindElixirTraders.Repo
+  alias NorthwindElixirTraders.{Repo, Country}
 
   def validate_foreign_key_id(changeset, target, field) when is_atom(field) do
     val = get_field(changeset, field)
@@ -24,6 +24,26 @@ defmodule NorthwindElixirTraders.Validations do
         _ ->
           changeset
       end
+    end
+  end
+
+  def validate_country(changeset, field) when is_atom(field) do
+    case {changeset.errors[field], get_field(changeset, field)} do
+      {nil, country} when not is_nil(country) ->
+        if is_nil(Country.get_dial(country)) do
+          add_error(
+            changeset,
+            field,
+            "country '%{country}' not found in the Countries table",
+            country: country,
+            validation: :country
+          )
+        else
+          changeset
+        end
+
+      _ ->
+        changeset
     end
   end
 end
