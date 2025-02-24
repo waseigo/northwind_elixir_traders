0882d41be333a07666d74ed22b97435501526212
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-24 05:08:41 +0200

    alias PhoneNumbers

--- .iex.exs
+++ .iex.exs
@@ -1,4 +1,4 @@
-alias NorthwindElixirTraders.{Repo,Category,Employee,Supplier,Product,Validations,DataImporter,Shipper}
+alias NorthwindElixirTraders.{Repo,Category,Employee,Supplier,Product,Validations,DataImporter,Shipper,PhoneNumbers}
 
 import Ecto.Query
 
