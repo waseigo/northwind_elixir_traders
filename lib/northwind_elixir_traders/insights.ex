23f77a97babc4c1f362a8d508f960c39becaf504
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-26 00:56:59 +0200

    The refactored Insights module with a function to calculate the total value of an Order

--- lib/northwind_elixir_traders/insights.ex
+++ lib/northwind_elixir_traders/insights.ex
@@ -2,10 +2,24 @@ defmodule NorthwindElixirTraders.Insights do
   import Ecto.Query
   alias NorthwindElixirTraders.{Repo, Product, Order, OrderDetail}
 
-  def query_order_detail_values(order_id) do
-    OrderDetail
-    |> join(:inner, [od], p in Product, on: od.product_id == p.id)
+  def query_order_details_by_order(order_id) do
+    join(OrderDetail, :inner, [od], p in Product, on: od.product_id == p.id)
     |> where([od], od.order_id == ^order_id)
+  end
+
+  def query_order_detail_values(order_id) do
+    query_order_details_by_order(order_id)
     |> select([od, p], od.quantity * p.price)
+  end
+
+  def query_order_total_value(order_id) do
+    query_order_details_by_order(order_id)
+    |> select([od, p], sum(od.quantity * p.price))
+  end
+
+  def calculate_order_value(%Order{id: order_id}), do: calculate_order_value(order_id)
+
+  def calculate_order_value(order_id) when not is_map(order_id) do
+    order_id |> query_order_total_value() |> Repo.one()
   end
 end
