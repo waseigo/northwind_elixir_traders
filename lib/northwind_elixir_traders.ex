baf3fba0094ee8fa3b0bc808b44c6ec0628aca24
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-25 21:00:13 +0200

    Moving the functions to get the application and all its modules (compiled and loaded) into northwind_elixir_traders.ex

--- lib/northwind_elixir_traders.ex
+++ lib/northwind_elixir_traders.ex
@@ -1,18 +1,31 @@
 defmodule NorthwindElixirTraders do
-  @moduledoc """
-  Documentation for `NorthwindElixirTraders`.
-  """
+  def get_application do
+    app = Application.get_application(__MODULE__)
+    s = app |> to_string |> Macro.camelize()
+    c = s |> String.to_atom()
+    %{atom: app, string: s, camelized: c}
+  end
 
-  @doc """
-  Hello world.
+  def get_compiled_modules do
+    get_application()
+    |> Map.get(:atom)
+    |> :application.get_key(:modules)
+    |> elem(1)
+  end
 
-  ## Examples
+  def get_loaded_modules do
+    :code.all_loaded() |> Enum.map(&elem(&1, 0))
+  end
 
-      iex> NorthwindElixirTraders.hello()
-      :world
+  def get_own_modules do
+    app = get_application()
 
-  """
-  def hello do
-    :world
+    get_compiled_modules()
+    |> Kernel.++(get_loaded_modules())
+    |> Enum.uniq()
+    |> List.delete(__MODULE__)
+    |> Enum.filter(fn m ->
+      Atom.to_string(m) |> String.contains?(app.string)
+    end)
   end
 end
