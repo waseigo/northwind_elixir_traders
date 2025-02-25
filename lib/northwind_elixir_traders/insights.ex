44eececc59dcff5eb5fff8d19cfe0fbf72771aeb
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-26 00:00:24 +0200

    Query function that prepares the calculation of OrderDetail value (product quantity times its price)

--- lib/northwind_elixir_traders/insights.ex
+++ lib/northwind_elixir_traders/insights.ex
@@ -1,4 +1,11 @@
 defmodule NorthwindElixirTraders.Insights do
   import Ecto.Query
   alias NorthwindElixirTraders.{Repo, Product, Order, OrderDetail}
+
+  def query_order_detail_values(order_id) do
+    OrderDetail
+    |> join(:inner, [od], p in Product, on: od.product_id == p.id)
+    |> where([od], od.order_id == ^order_id)
+    |> select([od, p], od.quantity * p.price)
+  end
 end
