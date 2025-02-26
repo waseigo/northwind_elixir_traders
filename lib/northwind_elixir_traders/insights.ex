b0d7f498c1d60a65c7c8b363b97ac59e1019af38
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-26 02:41:00 +0200

    Calculating the total value of a list of orders in parallel

--- lib/northwind_elixir_traders/insights.ex
+++ lib/northwind_elixir_traders/insights.ex
@@ -1,6 +1,8 @@
 defmodule NorthwindElixirTraders.Insights do
   import Ecto.Query
   alias NorthwindElixirTraders.{Repo, Product, Order, OrderDetail}
+  @timeout 10_000
+  @max_concurrency System.schedulers_online()
 
   def query_order_details_by_order(order_id) do
     join(OrderDetail, :inner, [od], p in Product, on: od.product_id == p.id)
@@ -21,5 +23,21 @@ defmodule NorthwindElixirTraders.Insights do
 
   def calculate_order_value(order_id) when not is_map(order_id) do
     order_id |> query_order_total_value() |> Repo.one()
+  end
+
+  def calculate_total_value_of_orders(orders, opts \\ [max_concurrency: @max_concurrency])
+      when is_list(orders) and is_list(opts) do
+    mc =
+      if Keyword.has_key?(opts, :max_concurrency),
+        do: Keyword.get(opts, :max_concurrency),
+        else: @max_concurrency
+
+    Task.async_stream(orders, &calculate_order_value/1,
+      ordered: false,
+      timeout: @timeout,
+      max_concurrency: mc
+    )
+    |> Enum.to_list()
+    |> Enum.reduce(0.0, fn {_status, value}, acc -> acc + value end)
   end
 end
