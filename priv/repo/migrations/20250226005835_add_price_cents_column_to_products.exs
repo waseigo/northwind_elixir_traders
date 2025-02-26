1996f5353532e7a8637dd8ef692f61fcd545e063
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-26 03:01:13 +0200

    Adding SQL execution to the AddPriceCentsColumnToProducts migration to convert dollar amounts to cents

--- priv/repo/migrations/20250226005835_add_price_cents_column_to_products.exs
+++ priv/repo/migrations/20250226005835_add_price_cents_column_to_products.exs
@@ -5,5 +5,7 @@ defmodule NorthwindElixirTraders.Repo.Migrations.AddPriceCentsColumnToProducts d
     alter table(:products) do
       add(:price_cents, :integer)
     end
+
+    execute("UPDATE products SET price_cents = price*100")
   end
 end
