690ced174d5fc4c2249c4675f5c52ce1670a56c6
Isaak Tsalicoglou <isaak@waseigo.com>
2025-02-26 03:09:54 +0200

    Splitting the change macro of the AddPriceCentsColumnToProducts migration into up and down actions

--- priv/repo/migrations/20250226005835_add_price_cents_column_to_products.exs
+++ priv/repo/migrations/20250226005835_add_price_cents_column_to_products.exs
@@ -1,11 +1,15 @@
 defmodule NorthwindElixirTraders.Repo.Migrations.AddPriceCentsColumnToProducts do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:products) do
       add(:price_cents, :integer)
     end
 
-    execute("UPDATE products SET price_cents = price*100")
+    execute("UPDATE products SET price_cents = CAST(price*100 AS INTEGER)")
+  end
+
+  def down do
+    alter(table(:products), do: remove(:price_cents))
   end
 end
