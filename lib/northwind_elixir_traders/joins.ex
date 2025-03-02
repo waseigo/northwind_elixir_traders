a915be15b3b47539e15f84d319633901ce47de23
Isaak Tsalicoglou <isaak@waseigo.com>
2025-03-03 01:48:46 +0200

    Refactored base xy/2 functions for joining LHS and RHS

--- lib/northwind_elixir_traders/joins.ex
+++ lib/northwind_elixir_traders/joins.ex
@@ -108,21 +108,11 @@ defmodule NorthwindElixirTraders.Joins do
     Module.split(m) |> List.last() |> String.downcase() |> String.to_atom()
   end
 
-  def xy(xm, ym) when xm in @lhs and ym in @rhs do
-    from(x in xm, as: :x)
-    |> join(:inner, [x: x], p in assoc(x, :products), as: :p)
-    |> join(:inner, [p: p], od in assoc(p, :order_details), as: :od)
-    |> join(:inner, [od: od], o in assoc(od, :order), as: :o)
-    |> join(:inner, [o: o], y in assoc(o, ^module_to_assoc_field(ym)), as: :y)
-  end
+  def xy(xm, ym) when xm in @lhs and ym in @rhs,
+    do: join_lhs_to_od(xm) |> join_od_to_order() |> join_rhs_to_order(ym)
 
-  def xy(xm, ym) when xm in @rhs and ym in @lhs do
-    from(x in xm, as: :x)
-    |> join(:inner, [x: x], o in assoc(x, :orders), as: :o)
-    |> join(:inner, [o: o], od in assoc(o, :order_details), as: :od)
-    |> join(:inner, [od: od], p in assoc(od, :product), as: :p)
-    |> join(:inner, [p: p], y in assoc(p, ^module_to_assoc_field(ym)), as: :y)
-  end
+  def xy(xm, ym) when xm in @rhs and ym in @lhs,
+    do: join_rhs_to_od(xm) |> join_od_to_product() |> join_lhs_to_product(ym)
 
   def join_od_to_product(query = %Ecto.Query{}),
     do: join(query, :inner, [od: od], p in assoc(od, :product), as: :p)
