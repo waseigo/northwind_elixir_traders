0043c19ee9c29674191e60955ddb44a7618e3376
Isaak Tsalicoglou <isaak@waseigo.com>
2025-03-14 00:55:04 +0200

    Migration that creates new indexes to increase the performance of joins, grouping and ordering

---
+++ priv/repo/migrations/20250313224348_add_query_optimization_indexes.exs
@@ -0,0 +1,14 @@
+defmodule NorthwindElixirTraders.Repo.Migrations.AddQueryOptimizationIndexes do
+  use Ecto.Migration
+
+  def change do
+    create index(:orders, [:date])
+    create index(:order_details, [:order_id])
+    create index(:order_details, [:product_id])
+    create index(:products, [:category_id])
+    create index(:products, [:supplier_id])
+    create index(:orders, [:employee_id])
+    create index(:orders, [:customer_id])
+    create index(:orders, [:shipper_id])
+  end
+end
